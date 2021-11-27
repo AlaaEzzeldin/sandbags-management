0b932c1255d9ddc3534690629585e7f22a0cb048
Valeria Stromtsova <v.stromtsova@yandex.ru>
2021-11-27 14:49:49 +0100

    feat Added fields to login page
    commit to be squashed

--- sandsack-management-frontend/src/views/LoginPage.vue
+++ sandsack-management-frontend/src/views/LoginPage.vue
@@ -1,26 +1,44 @@
 <template>
-  <v-form v-model="valid">
-    <v-text-field
-        v-model="email"
-        :rules="emailRules"
-        label="E-mail"
-        required
-    ></v-text-field>
-    <v-text-field
-        v-model="password"
-        :rules="passwordRules"
-        label="Password"
-        required
-    ></v-text-field>
-    <v-btn
-        :disabled="!valid"
-        color="success"
-        class="mr-4"
-        @click="validate"
-    >
-      Einloggen
-    </v-btn>
-  </v-form>
+  <v-container>
+    <v-row>
+      <v-col cols="4">
+        <v-form v-model="valid">
+          <h2>Einloggen bei der</h2>
+          <h2>Feuerwehr Passau</h2>
+          <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              label="E-mail"
+              required
+          ></v-text-field>
+          <v-text-field
+              v-model="password"
+              :rules="passwordRules"
+              label="Password"
+              required
+          ></v-text-field>
+          <v-checkbox
+              v-model="checkbox"
+              :label="`Angemeldet bleiben`"
+          ></v-checkbox>
+          <v-btn text>
+            <u>Passwort vergessen?</u>
+          </v-btn>
+          <v-btn
+              :disabled="!valid"
+              color="success"
+              class="mr-4"
+              @click="validate"
+          >
+            Einloggen
+          </v-btn>
+          <v-btn text>
+            Sie haben noch kein Konto? <u>Registrieren</u>
+          </v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
