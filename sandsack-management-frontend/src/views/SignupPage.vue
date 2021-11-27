fc1ebe63f539f9a5b534bd88fe20dc78aaf41879
Valeria Stromtsova <v.stromtsova@yandex.ru>
2021-11-27 15:14:24 +0100

    feat Added fields to sign up page

--- sandsack-management-frontend/src/views/SignupPage.vue
+++ sandsack-management-frontend/src/views/SignupPage.vue
@@ -1,32 +1,61 @@
 <template>
-  <v-form v-model="valid">
-    <v-text-field
-        v-model="email"
-        :rules="emailRules"
-        label="E-mail"
-        required
-    ></v-text-field>
-    <v-text-field
-        v-model="password"
-        :rules="passwordRules"
-        label="Password"
-        required
-    ></v-text-field>
-    <v-text-field
-        v-model="password"
-        :rules="passwordRules"
-        label="Repeat password"
-        required
-    ></v-text-field>
-    <v-btn
-        :disabled="!valid"
-        color="success"
-        class="mr-4"
-        @click="validate"
-    >
-      Anmelden
-    </v-btn>
-  </v-form>
+  <v-container>
+    <v-row>
+      <v-col cols="4">
+        <v-form v-model="valid">
+          <h2>Anmelden bei der</h2>
+          <h2>Feuerwehr Passau</h2>
+          <v-text-field
+              v-model="email"
+              :rules="[v => !!v || 'Email is required']"
+              label="E-mail"
+              required
+          ></v-text-field>
+          <v-text-field
+              v-model="password"
+              :rules="[v => !!v || 'Password is required']"
+              label="Password"
+              required
+          ></v-text-field>
+          <v-text-field
+              v-model="password"
+              :rules="[v => !!v || 'Password is required']"
+              label="Repeat password"
+              required
+          ></v-text-field>
+          <v-select
+              v-model="select"
+              :items="rolleList"
+              :rules="[v => !!v || 'Rolle is required']"
+              label="Rolle"
+              required
+          ></v-select>
+          <v-select
+              v-model="select"
+              :items="abschnittnameList"
+              :rules="[v => !!v || 'Abschnittname is required']"
+              label="Abschnittname"
+              required
+          ></v-select>
+          <v-checkbox
+              v-model="checkbox"
+              :label="`Angemeldet bleiben`"
+          ></v-checkbox>
+          <v-btn text>
+            <u>Zur Login-Seite</u>
+          </v-btn>
+          <v-btn
+              :disabled="!valid"
+              color="success"
+              class="mr-4"
+              @click="validate"
+          >
+            Anmelden
+          </v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -35,7 +64,18 @@ export default {
   name: 'SignupPage',
 
   data: () => ({
-
+    abschnittnameList: [
+      'Hauptabschintt-Mitte',
+      'EA 1-Altstadt',
+      'EA 1.1 Altstadt- Ost',
+      'Mollnhof',
+    ],
+    rolleList: [
+        'Unterabschnittleiter',
+        'Einsatzabschnittleiter',
+        'Hauptabschnittleiter',
+        'Mollnhofleiter',
+    ]
   }),
 
 }
