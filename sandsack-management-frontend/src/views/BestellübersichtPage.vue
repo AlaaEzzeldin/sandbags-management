73c2aeb206f888f767f78ad0bd07ac42fb1d71db
Valeria Stromtsova <v.stromtsova@yandex.ru>
2021-11-27 14:26:35 +0100

    Revert "report generato, status and graph"
    This reverts commit ffe0b7b78c7295d05f88f6634e1c8b9033f3f50e.

--- sandsack-management-frontend/src/views/BestellübersichtPage.vue
+++ sandsack-management-frontend/src/views/BestellübersichtPage.vue
@@ -1,111 +1,14 @@
 <template>
-  <div>
-    <v-row no-gutters>
-      <v-col sm="3" class="pt-13 justify-center align-center">
-        <h1 style="font-weight: bolder;">Bestellübersicht</h1>
-      </v-col>
-      <v-spacer></v-spacer>
-      <v-col sm="2" class="pt-15 justify-center align-center">
-
-        <v-select
-            :items="items"
-            label="Tage"
-            solo
-        ></v-select>
-
-      </v-col>
-    </v-row>
-    <v-row no-gutters>
-      <v-text-field
-          v-model="first"
-          value="10.00"
-          label="Bestellungen"
-          filled
-      ></v-text-field>
-
-      <v-text-field
-          v-model="second"
-          value="10.00"
-          label="Bestellungen bestatigt"
-          filled
-      ></v-text-field>
-
-      <v-text-field
-          v-model="third"
-          value="10.00"
-          label="Bestellungen/Uhr"
-          filled
-      ></v-text-field>
-      <v-col cols="12">
-        <Bestellübersicht class="mt-10"></Bestellübersicht>
-      </v-col>
-
-      <input v-model="message" placeholder="Bestellungen">
-      <v-spacer></v-spacer>
-      <v-select
-          :Abschnitt="Abschnitt"
-          label="Beim Abschnitt"
-          solo
-      ></v-select>
-
-      <v-sparkline
-          :value="value"
-          :gradient="gradient"
-          :smooth="radius || false"
-          :padding="padding"
-          :line-width="width"
-          :stroke-linecap="lineCap"
-          :gradient-direction="gradientDirection"
-          :fill="fill"
-          :type="type"
-          :auto-line-width="autoLineWidth"
-          auto-draw
-      ></v-sparkline>
-    </v-row>
-
-    <div class="text-right">
-      <v-btn
-          rounded
-          color="red"
-          dark
-      >
-        PDF Export
-      </v-btn>
-    </div>
-
-  </div>
-
+  <h1> Bestellübersicht Page </h1>
 </template>
-
 
 <script>
 
-
-const gradients = [
-  ['#222'],
-  ['#42b3f4'],
-  ['red', 'orange', 'yellow'],
-  ['purple', 'violet'],
-  ['#00c6ff', '#F0F', '#FF0'],
-  ['#f72047', '#ffd200', '#1feaea'],
-]
-
 export default {
-  data: () => ({
-    items: ['Heute', 'Gestern',],
-    width: 2,
-    radius: 10,
-    padding: 8,
-    lineCap: 'round',
-    gradient: gradients[5],
-    value: [0, 5, 8, 20, 5, 10, 3, 5, 1, 8, 2, 9, 5, 0,],
-    gradientDirection: 'top',
-    gradients,
-    fill: false,
-    type: 'trend',
-    autoLineWidth: false,
-  }),
+  name: 'BestellübersichtPage',
+
+  components: {
+
+  },
 }
 </script>
-
-
