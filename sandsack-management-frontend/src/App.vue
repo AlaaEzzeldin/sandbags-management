b0ac0b2273e98b5b71cf1f7d7c8a64fd2dd84606
AlaaEzzeldin <khalila@fim.uni-passau.de>
2021-11-28 19:38:49 +0100

    enhance login ui- colors and alignment

--- sandsack-management-frontend/src/App.vue
+++ sandsack-management-frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-main>
-      <v-container class="ma-0 pa-0" style="background-color: white">
+      <v-container style="background-color: white">
             <router-view style="background: white;" :key="$route.path"></router-view>
       </v-container>
     </v-main>
--- sandsack-management-frontend/src/views/LoginPage.vue
+++ sandsack-management-frontend/src/views/LoginPage.vue
@@ -1,43 +1,88 @@
 <template>
   <v-container fluid fill-height>
-    <v-row align="center" justify="center" >
-      <v-col cols="4" >
-        <v-card elevation="0" class="justify-center align-center align-content-center"  max-width="600px" >
-          <h2>Einloggen bei der</h2>
-          <h2>Feuerwehr Passau</h2>
+    <v-row align="center" justify="center">
+      <v-col cols="5">
+        <v-card elevation="0" class="justify-center align-center mt-16 pt-16 ">
+          <v-card-title class=" ml-10 pl-3">
+            <h1 style="color: black;font-weight: bolder;  font-size: x-large">Einloggen bei der</h1>
+          </v-card-title>
+          <v-card-subtitle class=" ml-10 pl-3">
+            <h1 style="color: red;font-weight: bolder;  font-size: x-large"> Feuerwehr Passau </h1>
+          </v-card-subtitle>
 
-          <v-form ref="form" v-model="valid" style="margin-top: 20px">
-            <v-text-field
-                v-model="email"
-                :rules="emailRules"
-                label="E-mail"
-                required
-            ></v-text-field>
-            <v-text-field
-                v-model="password"
-                :rules="passwordRules"
-                label="Password"
-                required
-            ></v-text-field>
-            <v-checkbox
-                v-model="checkbox"
-                :label="`Angemeldet bleiben`"
-            ></v-checkbox>
-            <v-btn text link :to="{name: links.recoverPasswordPage}">
-              <u>Passwort vergessen?</u>
-            </v-btn>
-            <v-btn
-                color="success"
-                class="mr-4"
-                @click="validate"
-                :to="`/orders-list/`+getUserRole()"
-            >
-              Einloggen
-            </v-btn>
-            Sie haben noch kein Konto?
-            <v-btn text link :to="{name: links.signupPage}">
-              <u>Registrieren</u>
-            </v-btn>
+          <v-form ref="form" v-model="valid" class=" ma-10 pa-3">
+
+            <v-row>
+              <v-col>
+                <v-text-field
+                    v-model="email"
+                    :rules="emailRules"
+                    label="E-mail"
+                    required
+                    filled
+                    outlined
+                    prepend-icon="mdi-email"
+
+                ></v-text-field>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col>
+                <v-text-field
+                    v-model="password"
+                    :rules="passwordRules"
+                    label="Password"
+                    required
+                    filled
+                    outlined
+                    prepend-icon="mdi-lock"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+
+            <v-row align-content="center" justify="center">
+              <v-col class="text-left" cols="6">
+                <v-checkbox
+                    v-model="checkbox"
+                    :label="`Angemeldet bleiben`"
+                    class="justify-center align-center mt-0"
+                ></v-checkbox>
+              </v-col>
+              <v-col class="text-right" cols="6">
+                <v-btn text link :to="{name: links.recoverPasswordPage}" small>
+                  <u>Passwort vergessen?</u>
+                </v-btn>
+              </v-col>
+            </v-row>
+
+            <v-row no-gutters>
+              <v-col>
+                <v-btn
+                    style="text-transform: capitalize; font-weight: bolder;"
+                    color="red"
+                    dark
+                    block
+                    class="mr-4"
+                    @click="validate"
+                    :to="`/orders-list/`+getUserRole()"
+                >
+                  Einloggen
+                </v-btn>
+              </v-col>
+            </v-row>
+
+            <v-row class="text-right">
+              <v-col>
+                Sie haben noch kein Konto?
+                <v-btn text link :to="{name: links.signupPage}"
+                       small
+                >
+                  <u>Registrieren</u>
+                </v-btn>
+              </v-col>
+            </v-row>
+
           </v-form>
         </v-card>
       </v-col>
