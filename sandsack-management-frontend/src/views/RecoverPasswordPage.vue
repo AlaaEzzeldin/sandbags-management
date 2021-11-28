9f74798deba5ab96c31ea6d3301867011a1981c0
AlaaEzzeldin <khalila@fim.uni-passau.de>
2021-11-28 20:27:57 +0100

    enhance sign up and recover password  UI colors and alignment

--- sandsack-management-frontend/src/views/RecoverPasswordPage.vue
+++ sandsack-management-frontend/src/views/RecoverPasswordPage.vue
@@ -1,29 +1,52 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col cols="4">
-        <v-form v-model="valid">
-          <h2>Passwort</h2>
-          <h2>vergessen?</h2>
-          <v-text-field
-              v-model="email"
-              :rules="emailRules"
-              label="E-mail"
-              required
-          ></v-text-field>
-          <v-btn
-              :disabled="!valid"
-              color="success"
-              class="mr-4"
-              @click="validate"
-          >
-            Passwort wiederherstellen
-          </v-btn>
-          <v-btn text link :to="{name: links.logInPage}">
-            <u>Zur Login-Seite</u>
-          </v-btn>
-        </v-form>
+    <v-row align="center" justify="center">
+      <v-col cols="4 ">
+        <v-card elevation="0" class="justify-center align-center mt-16 pt-16 ">
+          <v-card-title class=" ml-10 pl-3">
+            <h1 style="color: black;font-weight: bolder;  font-size: x-large">Passwort vergessen?</h1>
+          </v-card-title>
+          <v-card-text>
+            <v-form v-model="valid">
+              <v-text-field
+                  v-model="email"
+                  :rules="emailRules"
+                  label="E-mail"
+                  required
+                  filled
+                  outlined
+                  prepend-icon="mdi-email"
+              ></v-text-field>
+              <v-row no-gutters>
+                <v-col >
+                  <v-btn
+                      style="text-transform: capitalize; font-weight: bolder;"
+                      block
+                      dark
+                      color="red"
+                      @click="validate"
+                  >
+                    Passwort wiederherstellen
+                  </v-btn>
+                </v-col>
+              </v-row>
+              <v-row >
+                <v-col class="text-right">
+                  <v-btn text small @click="goToLogin ">
+                    <u>Zur Login-Seite</u>
+                  </v-btn>
+                </v-col>
+
+              </v-row>
+
+            </v-form>
+
+          </v-card-text>
+        </v-card>
       </v-col>
+    </v-row>
+    <v-row>
+
     </v-row>
   </v-container>
 </template>
@@ -44,6 +67,15 @@ export default {
       logInPage: "LoginPage",
     }
   }),
+
+  methods:
+      {
+        goToLogin(){
+          this.$router.push({name: 'LoginPage'})
+        }
+
+      }
+
 
 }
 </script>
