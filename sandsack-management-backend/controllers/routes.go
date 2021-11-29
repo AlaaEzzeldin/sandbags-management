675f1c21d7098d542868e049132d95e74aa773ed
Anastasia Penkova <anastasia@salufast.com>
2021-11-29 23:55:20 +0100

    define api endpoints

--- sandsack-management-backend/controllers/routes.go
+++ sandsack-management-backend/controllers/routes.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"io"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -23,9 +24,72 @@ func (a *App) RunAllRoutes(){
 	log.SetOutput(f)
 	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
 
-	r.POST("/login", a.Login)
-	r.POST("/refresh", a.RefreshAccessToken)
 	r.POST("/hello", a.Hello)
+
+	r.POST("/users/login", a.Login)
+	r.POST("/users/refresh", a.RefreshAccessToken)
+	r.POST("/users", func(context *gin.Context) {
+		context.JSON(http.StatusNoContent, gin.H{
+			"message": "in development",
+		})
+	})
+	r.POST("/users", func(context *gin.Context) {
+		context.JSON(http.StatusNoContent, gin.H{
+			"message": "in development",
+		})
+	})
+	r.POST("/users/me", func(context *gin.Context) {
+		context.JSON(http.StatusNoContent, gin.H{
+			"message": "in development",
+		})
+	})
+	r.POST("/users/logout", func(context *gin.Context) {
+		context.JSON(http.StatusNoContent, gin.H{
+			"message": "in development",
+		})
+	})
+	r.POST("/users/forgot_password", func(context *gin.Context) {
+		context.JSON(http.StatusNoContent, gin.H{
+			"message": "in development",
+		})
+	})
+	r.POST("/users/change_password", func(context *gin.Context) {
+		context.JSON(http.StatusNoContent, gin.H{
+			"message": "in development",
+		})
+	})
+	r.POST("/users/verify", func(context *gin.Context) {
+		context.JSON(http.StatusNoContent, gin.H{
+			"message": "in development",
+		})
+	})
+	r.POST("/users/activation", func(context *gin.Context) {
+		context.JSON(http.StatusNoContent, gin.H{
+			"message": "in development",
+		})
+	})
+	r.POST("/users/email_verification", func(context *gin.Context) {
+		context.JSON(http.StatusNoContent, gin.H{
+			"message": "in development",
+		})
+	})
+
+	r.POST("/order", func(context *gin.Context) {
+		context.JSON(http.StatusNoContent, gin.H{
+			"message": "in development",
+		})
+	})
+
+	r.POST("/order/upgrade", func(context *gin.Context) {
+		context.JSON(http.StatusNoContent, gin.H{
+			"message": "in development",
+		})
+	})
+	r.POST("/order/cancel", func(context *gin.Context) {
+		context.JSON(http.StatusNoContent, gin.H{
+			"message": "in development",
+		})
+	})
 
 	//authorized := r.Use(AuthorizeJWT())
 	//authorized.POST("/registration", a.Registration)
