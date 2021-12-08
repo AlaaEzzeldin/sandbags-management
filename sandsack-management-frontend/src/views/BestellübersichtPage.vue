8534d2f9094fc9db8e52d5c8e7f525f86983d9cb
shinejose-PC\shinejose <shine.jose3@gmail.com>
2021-12-08 06:46:00 +0100

    calendar added, simple graph added alignment needed, again the center component color changed

--- sandsack-management-frontend/src/views/BestellübersichtPage.vue
+++ sandsack-management-frontend/src/views/BestellübersichtPage.vue
@@ -1,102 +1,114 @@
 <template>
   <div>
-   <v-row no-gutters>
-     <v-col sm="3" class="pt-13 justify-center align-center">
-       <h1 style="font-weight: bolder;">Bestellübersicht</h1>
-     </v-col>
-     <v-spacer></v-spacer>
-     <v-col sm="2" class="pt-15 justify-center align-center">
-       <v-select
-           :items="items"
-           filled
-           outlined
-           label="Tage"
-           solo
-       ></v-select>
-     </v-col>
-   </v-row>
-   <v-row no-gutters>
-     <v-toolbar>
-       <v-tabs
-           dark
-           background-color="grey"
-           outlined
-           grow
-       >
-         <v-tab>
-           <v-badge
-               color="green"
-               outlined
-               content="35">
-             Bestellungen
-           </v-badge>
-         </v-tab>
+    <v-row>
+      <v-col sm="3" class="pt-13 justify-center align-center">
+        <h1 style="font-weight: bolder;">Bestellübersicht</h1>
+      </v-col>
+      <v-spacer></v-spacer>
 
-         <v-tab>
-           <v-badge
-               color="green"
-               content="94%"
-               outlined
-           >
-             bestellungen Bestatigt
-           </v-badge>
-         </v-tab>
 
-         <v-tab>
-           <v-badge
-               color="green"
-               content="5"
-               outlined
-           >
-             bestellungen/Uhr
-           </v-badge>
-         </v-tab>
-       </v-tabs>
-     </v-toolbar>
-     <v-col cols="12">
-       <Bestellübersicht class="mt-10"></Bestellübersicht>
-     </v-col>
-     <v-container fluid>
-       <v-row align="center">
-         <v-col cols="6">
-           <v-subheader>
-             Bestellungen
-           </v-subheader>
-         </v-col>
+      <v-col sm="4" class="pt-15 justify-center align-center">
+        <v-menu
+            v-model="menu"
+            :close-on-content-click="false"
+            :nudge-right="50"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+                v-model="date"
+                label="Tage"
+                filled
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker
+              v-model="date"
+              @input="menu = false"
+          ></v-date-picker>
+        </v-menu>
+      </v-col>
+    </v-row>
 
-         <v-col cols="6">
-           <v-select
-               v-model="select"
-               :items="amount"
-               item-text="state"
-               label="Beim Abschnitt"
-               filled
-               outlined
-               persistent-hint
-               return-object
-               single-line
-           ></v-select>
-         </v-col>
-       </v-row>
-     </v-container>
-     <v-sparkline
-         :value="value"
-         :gradient="gradient"
-         :smooth="radius || false"
-         :padding="padding"
-         :line-width="width"
-         :stroke-linecap="lineCap"
-         :gradient-direction="gradientDirection"
-         :fill="fill"
-         :type="type"
-         :auto-line-width="autoLineWidth"
-         auto-draw
-     ></v-sparkline>
-   </v-row>
+    <v-row no-gutters>
+      <v-toolbar>
+        <v-tabs
+            dark
+            background-color="grey"
+            outlined
+            grow
+        >
+          <v-tab>
+            <v-badge
+                color="red"
+                outlined
+                content="35">
+              Bestellungen
+            </v-badge>
+          </v-tab>
+
+          <v-tab>
+            <v-badge
+                color="red"
+                content="94%"
+                outlined
+            >
+              bestellungen Bestatigt
+            </v-badge>
+          </v-tab>
+
+          <v-tab>
+            <v-badge
+                color="red"
+                content="5"
+                outlined
+            >
+              bestellungen/Uhr
+            </v-badge>
+          </v-tab>
+        </v-tabs>
+      </v-toolbar>
+      <v-col cols="12">
+        <Bestellübersicht class="mt-10"></Bestellübersicht>
+      </v-col>
+
+      <v-container fluid>
+        <v-row align="center">
+          <v-col cols="6">
+            <v-subheader>
+              Bestellungen
+            </v-subheader>
+          </v-col>
+
+          <v-col cols="6">
+            <v-select
+                v-model="select"
+                :items="amount"
+                item-text="state"
+                label="Beim Abschnitt"
+                filled
+                outlined
+                persistent-hint
+                return-object
+                single-line
+            ></v-select>
+          </v-col>
+        </v-row>
+        <GChart type="LineChart" :data="chartData" :options="chartOptions" />
+      </v-container>
+
+    </v-row>
+    <v-spacer></v-spacer>
     <v-card-actions>
       <v-row>
-        <v-col cols="12" sm="6" offset="3">
+        <v-col cols="12" sm="3" offset="9">
           <v-btn
+              align="right"
               style="text-transform: capitalize; font-weight: bolder;"
               block
               rounded
@@ -107,45 +119,61 @@
           </v-btn>
         </v-col>
       </v-row>
-
     </v-card-actions>
-   </div>
+  </div>
 </template>
 
+
+
+
 <script>
-const gradients = [
- ['#222'],
- ['#42b3f4'],
- ['red', 'orange', 'yellow'],
- ['purple', 'violet'],
- ['#00c6ff', '#F0F', '#FF0'],
- ['#f72047', '#ffd200', '#1feaea'],
-]
+import { GChart } from "vue-google-charts";
 export default {
- data: () => ({
+  name: "App",
+  components: {
+    GChart,
+  },
+  data() {
+    return {
+
+      date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+      modal: false,
+      menu: false,
+
+      // Array will be automatically processed with visualization.arrayToDataTable function
+      chartData: [
+        ["period", "Status"],
+        ["5-10", 5],
+        ["10-20", 12],
+        ["20-25", 9],
+        ["25-30", 6],
+        ["30-50", 6],
+      ],
+
+      amount: [
+        { state: 'Sample1',},
+        { state: 'Sample2',},
+        { state: 'sample3',},
+        { state: 'sample4',},
+        { state: 'sample5',},
+      ],
 
 
-     amount: [
-       { state: 'Sample1',},
-       { state: 'Sample2',},
-       { state: 'sample3',},
-       { state: 'sample4',},
-       { state: 'sample5',},
-     ],
-
-   items: ['Heute', 'Gestern','Vor Gestern',],
-
-   width: 2,
-   radius: 10,
-   padding: 8,
-   lineCap: 'round',
-   gradient: gradients[5],
-   value: [0, 5, 8, 20, 5, 10, 3, 5, 1, 8, 2, 9, 5, 0,],
-   gradientDirection: 'top',
-   gradients,
-   fill: false,
-   type: 'trend',
-   autoLineWidth: false,
- }),
-}
+      chartOptions: {
+        hAxis: {
+          title: "Request",
+          titleTextStyle: { color: "#FF0000" },
+        },
+        vAxis: {
+          title: "Time",
+          titleTextStyle: { color: "#FF0000" }
+        },
+        chart: {
+          title: "order status",
+        },
+      },
+    };
+  },
+};
 </script>
+
