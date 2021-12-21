0891aac7e6ea391a872c592ef186e2854b01095a
shinejose-PC\shinejose <shine.jose3@gmail.com>
2021-12-21 20:38:41 +0100

    changes in the graph updated

--- sandsack-management-frontend/src/views/BestellübersichtPage.vue
+++ sandsack-management-frontend/src/views/BestellübersichtPage.vue
@@ -15,6 +15,7 @@
             offset-y
             min-width="auto"
         >
+
           <template v-slot:activator="{ on, attrs }">
             <v-text-field
                 v-model="dateRangeText"
@@ -197,21 +198,3 @@ export default {
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40pt 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
