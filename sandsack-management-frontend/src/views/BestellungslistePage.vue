187697d08cf705609fc8c3f5b86157118be4328f
Valeria Stromtsova <v.stromtsova@yandex.ru>
2021-12-23 23:10:33 +0300

    Fix Bestellung exportieren

--- sandsack-management-frontend/src/views/BestellungslistePage.vue
+++ sandsack-management-frontend/src/views/BestellungslistePage.vue
@@ -14,107 +14,6 @@
             dark
             block
         >
-          <button  @click="download">Exportiern</button>
-        </v-btn>
-        <v-btn
-            v-if="this.getLoggedInUserRole() === 4"
-            style="text-transform: capitalize; font-weight: bolder;"
-            rounded
-            color="primary"
-            dark
-            block
-        >
-          Lieferschein drucken
-        </v-btn>
-      </v-col>
-    </v-row>
-    <v-row no-gutters>
-      <v-col cols="12">
-        <Bestelltabelle class="mt-10"></Bestelltabelle>
-      </v-col>
-    </v-row>
-
-  </div>
-
-</template>
-
-<script>
-import jsPDF from "jspdf";
-import domtoimage from "dom-to-image";
-import Bestelltabelle from "@/components/Bestelltabelle";
-
-export default {
-  name: 'BestellungslistePage',
-  components: {
-    Bestelltabelle
-  },
-  computed:{
-
-  },
-  methods:{
-
-    download() {
-      /** WITH CSS */
-      domtoimage
-          .toPng(this.$refs.content)
-          .then(function(dataUrl) {
-            var img = new Image();
-            img.src = dataUrl;
-            const doc = new jsPDF({
-              orientation: "portrait",
-              unit: "pt",
-              format: [900, 1500]
-            });
-            doc.addImage(img, "JPEG", 100, 100);
-            const date = new Date();
-            const url = window.URL.createObjectURL;
-            const filename =
-                "Exportiern_" +
-                date.getFullYear() +
-                ("0" + (date.getMonth() + 1)).slice(-2) +
-                ("0" + date.getDate()).slice(-2) +
-                ("0" + date.getHours()).slice(-2) +
-                ".pdf";
-            doc.save(filename)
-            window.URL.revokeObjectURL(url);
-            alert("Exportiern Downloaded!"); // or you know, something with better UX...
-          })
-          .catch(function(error) {
-            console.error("oops, something went wrong!", error);
-          });
-    },
-
-
-    // hard coding the users roles
-    getLoggedInUserRole() {
-      if (this.$route.params.userRole === '1') // Hauptabschintt
-        return 1
-      else if (this.$route.params.userRole === '2') // Einzatsabschnitt
-        return 2
-      else if (this.$route.params.userRole === '3') //Unterabschnitt
-        return 3
-      else if (this.$route.params.userRole === '4') // Mollhof
-        return 4
-    }
-  }
-}
-</script>
-//<!-- <template>
-  <div ref="content">
-    <v-row no-gutters>
-      <v-col sm="3" class="pt-13 justify-center align-center">
-        <h1 style="font-weight: bolder;">Bestellungsliste</h1>
-      </v-col>
-      <v-spacer></v-spacer>
-      <v-col sm="2" class="pt-15 justify-center align-center">
-        <v-btn
-            v-if="this.getLoggedInUserRole()===1 || this.getLoggedInUserRole()===2"
-            style="text-transform: capitalize; font-weight: bolder;"
-            rounded
-            color="red"
-            dark
-            block
-        >
           <button  @click="pdfgen">Exportiern</button>
         </v-btn>
         <v-btn
@@ -148,18 +47,47 @@ export default {
   components: {
     Bestelltabelle
   },
-  computed:{
 
+  created() {
+    this.$store.dispatch("loadOrders")
+  },
+  computed: {
+    getOrders() {
+      return this.$store.getters.getOrders
+    }
   },
   methods:{
     pdfgen: function () {
       var pdfMake = require('pdfmake/build/pdfmake.js')
-      if (pdfMake.vfs == undefined){
+      if (pdfMake.vfs === undefined){
         var pdfFonts = require('pdfmake/build/vfs_fonts.js')
         pdfMake.vfs = pdfFonts.pdfMake.vfs;
       }
-      var docDefinition = { content: 'content' }
-      pdfMake.createPdf(docDefinition).download('Exportiern.pdf')
+      let body =  [[ 'id', 'Zeit', 'Von', 'Priorität', 'Status', 'Anschrift' ]];
+      for (let order of this.getOrders) {
+        body.push(
+            [
+              order.id,
+              order.created_at,
+              order.from,
+              order.priority,
+              order.status,
+              order.deliveryAddress
+            ]
+        );
+      }
+      var docDefinition = {
+        content: [
+          {
+            layout: 'lightHorizontalLines',
+            table: {
+              headerRows: 1,
+              body: body
+            }
+          }
+        ]
+      }
+      pdfMake.createPdf(docDefinition).download('Bestellungen.pdf')
     },
 
 
@@ -176,4 +104,4 @@ export default {
     }
   }
 }
-</script> //-->
+</script>
