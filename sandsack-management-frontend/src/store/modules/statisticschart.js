8ea12590f3c750f662081818f3331e99ac476b2b
shinejose-PC\shinejose <shine.jose3@gmail.com>
2022-01-07 09:49:51 +0100

    Logic To The Bestellungpage

---
+++ sandsack-management-frontend/src/store/modules/statisticschart.js
@@ -0,0 +1,39 @@
+import statisticschartAPI from "../../api/statisticschart";
+
+const state = {
+    statisticschart: [],
+}
+
+const getters = {
+    getStatisticschart(state) {
+
+        console.log(state.statisticschart);
+        return state.statisticschart;
+    },
+
+}
+
+const mutations = {
+    SET_STATISTICSCHART(state, statisticschart) {
+        state.statisticschart = statisticschart;
+    }
+};
+
+const actions = {
+    loadStatisticschart({commit}) {
+        statisticschartAPI.index()
+            .then(function (response) {
+                commit('SET_STATISTICSCHART', response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+};
+
+export default({
+    state,
+    mutations,
+    actions,
+    getters
+});
--- sandsack-management-frontend/src/views/BestellübersichtPage.vue
+++ sandsack-management-frontend/src/views/BestellübersichtPage.vue
@@ -281,7 +281,6 @@ export default {
         return 4
     }
   }
-
 };
 </script>
 
