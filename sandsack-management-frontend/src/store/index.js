7140f4a3eb5fef1c9cc6f9e7e9869b6ea05d08f4
shinejose-PC\shinejose <shine.jose3@gmail.com>
2022-01-09 13:04:22 +0100

    Modified Statistic Chart & Logic To BestellübersichtPage.vue

--- sandsack-management-frontend/src/store/index.js
+++ sandsack-management-frontend/src/store/index.js
@@ -3,13 +3,18 @@ import Vuex from 'vuex'
 import users from './modules/users'
 import orders from './modules/orders'
 import equipment from './modules/equipment'
+import statisticschart from './modules/statisticschart'
+
 
 Vue.use(Vuex)
 
 export default  new Vuex.Store({
-  modules: {
-    orders,
-    users,
-    equipment
-  }
+    modules: {
+        orders,
+        users,
+        equipment,
+        statisticschart,
+
+
+    }
 })
