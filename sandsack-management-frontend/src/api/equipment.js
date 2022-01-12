fe1cb84999dfef198d7b30f5ee55ef4b26606169
Valeria Stromtsova <v.stromtsova@yandex.ru>
2022-01-12 15:24:23 +0300

    Integration of equipment with backend

--- sandsack-management-frontend/src/api/equipment.js
+++ sandsack-management-frontend/src/api/equipment.js
@@ -1,13 +1,16 @@
 import axios from 'axios'
+import authHeader from "../services/auth-header";
+
 export default {
     index( params ){
-        return axios.get( 'http://localhost:3001/equipment', {
+        return axios.get( 'http://18.193.112.34:8001/core/equipment', {
+            headers: authHeader(),
             params: params
         })
     },
 
     update( id, data ){
-        return axios.patch( 'http://localhost:3001/equipment/'+id, data);
+        return axios.patch( 'https://localhost:3001/equipment/'+id, data);
     },
 
 }
