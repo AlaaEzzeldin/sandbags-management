e30877e7913c093321fcfa3e8aa0279ec106eb96
Valeria Stromtsova <v.stromtsova@yandex.ru>
2022-01-13 23:41:58 +0300

    Added navigation (responsive)

--- sandsack-management-frontend/src/components/Nav.vue
+++ sandsack-management-frontend/src/components/Nav.vue
@@ -1,43 +1,56 @@
 <template style="background-color: white">
-  <v-navigation-drawer
-      :permanent="$vuetify.breakpoint.smAndUp"
-      width="350"
-      app
-      class="logo pa-0 ma-0"
-  >
-    <div>
-      <v-row class="pt-10 pb-10 pl-5">
-        <v-col cols="3">
-          <img class="mr-10" src="@/assets/images/logo.png" height="60"/>
-        </v-col>
-        <v-col cols="9">
-          <h1 style="color: red;font-weight: bolder;  font-size: x-large" class="justify-center"> Feuerwehr Passau </h1>
-          <h1 style="color: black;font-weight: bolder;  font-size: large;" class="justify-center">
-            {{ getLoggedInBranchName() }} </h1>
-        </v-col>
-      </v-row>
-    </div>
+  <div>
+    <v-navigation-drawer
+        :permanent="$vuetify.breakpoint.smAndUp"
+        absolute
+        left
+        style="min-width: 350px;"
+        v-model="drawer"
+    >
+      <div class="hidden-sm-and-down">
+        <v-row class="pt-10 pb-10 pl-5">
+          <v-col cols="3">
+            <img class="mr-10" src="@/assets/images/logo.png" height="60"/>
+          </v-col>
+          <v-col cols="9">
+            <h1 style="color: red;font-weight: bolder;  font-size: x-large" class="justify-center"> Feuerwehr Passau </h1>
+            <h1 style="color: black;font-weight: bolder;  font-size: large;" class="justify-center">
+              {{ getLoggedInBranchName() }} </h1>
+          </v-col>
+        </v-row>
+      </div>
 
-    <v-list rounded class="mb-16">
-      <v-list-item-group>
-        <v-list-item
-            v-for="(item, i) in getNavListForLoggedInUserRoll()"
-            :key="i"
-            link
-            :to="{name:item.component}"
-        >
-          <v-icon style="color: red" v-text="item.icon"></v-icon>
-          <v-list-item-title
-              v-if="$vuetify.breakpoint.lgAndUp"
-              class="text-left ml-2"
-              style="font-weight: bolder;  font-size: large;"
-              v-text="item.title"
-          ></v-list-item-title>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
-    <EquipmentQuantityTable/>
-  </v-navigation-drawer>
+      <v-list class="mb-16 mt-16">
+          <v-list-item
+              v-for="(item, i) in getNavListForLoggedInUserRoll()"
+              :key="i"
+              link
+              :to="{name:item.component}"
+          >
+            <v-list-item-icon>
+              <v-icon style="color: red" v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title
+                  class="text-left ml-2"
+                  style="font-weight: bolder;  font-size: large;"
+                  v-text="item.title"
+              ></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+      </v-list>
+      <EquipmentQuantityTable/>
+    </v-navigation-drawer>
+    <v-toolbar
+         absolute
+         right
+         flat
+         class="hidden-sm-and-up"
+         @click.stop="drawer = !drawer">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      Feuerwehr Passau ({{ getLoggedInBranchName() }})
+    </v-toolbar>
+  </div>
 </template>
 
 <script>
@@ -52,6 +65,7 @@ export default {
 
   data() {
     return {
+      drawer: null,
       navItemsUnterabschintt: [
         {
           title: 'Bestellungliste',
