c9c1e58cc59c8545d37c37720c2248b2353554c9
AlaaEzzeldin <khalila@fim.uni-passau.de>
2022-01-13 20:44:45 +0100

    orders api

--- sandsack-management-frontend/src/api/orders.js
+++ sandsack-management-frontend/src/api/orders.js
@@ -1,13 +1,16 @@
 import axios from 'axios'
+import authHeader from "../services/auth-header";
+
 export default {
     index( params ){
-        return axios.get( 'http://localhost:3001/orders', {
+        return axios.get( 'http://18.193.112.34:8001/order', {
+            headers: authHeader(),
             params: params
         })
     },
 
     show( id ){
-        return axios.get( 'http://localhost:3001/orders/'+id );
+        return axios.get( 'http://localhost:3001/order/'+id );
     },
 
     update( id, data ){
