4c0cd7a7758db9882fb5190a828f5131025ae7eb
Valeria Stromtsova <v.stromtsova@yandex.ru>
2022-01-14 01:34:34 +0300

    Added password edit dialog

---
+++ sandsack-management-frontend/src/components/PasswordEditDialog.vue
@@ -0,0 +1,152 @@
+<template>
+  <v-dialog
+      v-model="dialog"
+      width="800"
+  >
+    <v-card outlined >
+      <v-card-title>
+        <v-col cols="2">
+          <v-avatar
+              color="white"
+              size="60">
+            <v-icon color="black" large>mdi-home</v-icon>
+          </v-avatar>
+        </v-col>
+        <v-col cols="10">
+          <h2>{{getLoggedInUser.name}}</h2>
+          <h3>{{getLoggedInUser.roleName}}</h3>
+        </v-col>
+      </v-card-title>
+      <v-card-text>
+        <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+        >
+<!--          <v-text-field
+              v-model="getLoggedInUser.email"
+              :rules="emailRules"
+              label="E-mail"
+              required
+              filled
+              outlined
+              prepend-icon="mdi-email"
+          ></v-text-field>-->
+          <v-text-field
+              v-model="getLoggedInUser.name"
+              :rules="nameRules"
+              label="Name"
+              required
+              filled
+              outlined
+              prepend-icon="mdi-phone"
+          ></v-text-field>
+          <v-text-field
+              v-model="getLoggedInUser.phone"
+              :rules="phoneRules"
+              label="Phone"
+              required
+              filled
+              outlined
+              prepend-icon="mdi-phone"
+          ></v-text-field>
+
+<!--          <v-text-field
+              v-model="getLoggedInUser.password"
+              :rules="passwordRules"
+              label="Altes Passwort"
+              prepend-icon="mdi-lock"
+              required
+              filled
+              outlined
+          ></v-text-field>
+          <v-text-field
+              v-model="getLoggedInUser.password"
+              :rules="passwordRules"
+              label="Neues Passwort"
+              prepend-icon="mdi-lock"
+              required
+              filled
+              outlined
+          ></v-text-field>-->
+        </v-form>
+      </v-card-text>
+        <v-card-actions>
+          <v-row>
+            <v-col class="align-center justify-center" cols="3" offset="3">
+              <v-btn
+                  style="text-transform: capitalize; font-weight: bolder;"
+                  block
+                  rounded
+                  color="red"
+                  dark
+                  @click="dialog = false"
+              >
+                Abrechen
+              </v-btn>
+
+            </v-col>
+            <v-col cols="3">
+
+              <v-btn
+                  style="text-transform: capitalize; font-weight: bolder;"
+                  block
+                  rounded
+                  color="red"
+                  dark
+                  @click="submitUpdatedInfo"
+              >
+                Speichern
+              </v-btn>
+            </v-col>
+
+          </v-row>
+
+        </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+
+export default {
+  name: 'PasswordEditDialog',
+  props: ['dialog'],
+
+  data() {
+    return {
+      valid: true,
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      phoneRules: [
+        v => !!v || 'Phone is required',
+        v => /\(?\+\(?49\)?[ ()]?([- ()]?\d[- ()]?){10}/.test(v) || 'Phone must be valid',
+      ],
+      nameRules: [
+        v => !!v || 'Name is required',
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+      ],
+    }
+  },
+
+  computed: {
+    getLoggedInUser() {
+      return this.$store.getters.getLoggedInUser
+    }
+    },
+  methods: {
+    submitUpdatedInfo(){
+      let data={
+        "name": this.getLoggedInUser.name,
+        "phone": this.getLoggedInUser.phone,
+      }
+      this.$store.dispatch("updateUserInfo",  data)
+      this.$emit("close")
+    }
+  }
+}
+</script>
