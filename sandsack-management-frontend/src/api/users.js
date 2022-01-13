aeb9d4ac0b18fb6fa202250891249b0492b9f804
AlaaEzzeldin <khalila@fim.uni-passau.de>
2022-01-13 21:38:33 +0100

    integrate the backend to the apis of user's info

--- sandsack-management-frontend/src/api/users.js
+++ sandsack-management-frontend/src/api/users.js
@@ -1,30 +1,26 @@
 import axios from 'axios'
 import authHeader from "../services/auth-header";
-//import {BASE_URL} from "../api/config";
+import {BASE_URL} from "../api/config";
 
 export default {
-    index( params ){
-        return axios.get( 'http://18.193.112.34:8001/users', {
+    index(params) {
+        return axios.get(BASE_URL + 'users', {
             headers: authHeader(),
             params: params
         })
     },
 
     show() {
-        return axios.get('http://18.193.112.34:8001/users/me', {
+        return axios.get(BASE_URL + 'users/me', {
             headers: authHeader(),
         })
     },
 
-    update( id, data ){
-        return axios.put( 'http://localhost:3001/users/'+id, data );
-    },
-
-    create( data ){
-        return axios.post( 'http://localhost:3001/users/', data );
-    },
-
-    delete( id ){
-        return axios.delete( 'http://localhost:3001/users/' + id )
+    update(data) {
+        console.log("userinfo", data)
+        return axios.patch(BASE_URL + 'users/me', data, {
+            headers: authHeader()
+        })
     }
+
 }
