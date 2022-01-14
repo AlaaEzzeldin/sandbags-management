21c751e593851b599e875ccda9952d5df029dca7
AlaaEzzeldin <khalila@fim.uni-passau.de>
2022-01-14 02:11:21 +0100

    write the new order apis

--- sandsack-management-frontend/src/api/orders.js
+++ sandsack-management-frontend/src/api/orders.js
@@ -3,19 +3,20 @@ import authHeader from "../services/auth-header";
 import {BASE_URL} from "../api/config";
 
 export default {
+
     index(params) {
         return axios.get(BASE_URL + 'order', {
             headers: authHeader(),
             params: params
         })
     },
-    show(id) {
-        return axios.get('http://localhost:3001/order/' + id);
-    },
 
-    update(id, data) {
-        return axios.patch('http://localhost:3001/orders/' + id, data);
-    },
+      show(id) {
+        return axios.get(BASE_URL + 'order'+ id, {
+            headers: authHeader(),
+        })
+     },
+
 
     create(data) {
         return axios.post(BASE_URL + 'order', data, {
@@ -23,7 +24,29 @@ export default {
         });
     },
 
-    delete(id) {
-        return axios.delete('http://localhost:3001/orders/' + id)
-    }
+    acceptOrder(id) {
+        return axios.post(BASE_URL + 'order/accept' + id, {
+            headers: authHeader(),
+        });
+    },
+    rejectOrder(id) {
+        return axios.post(BASE_URL + 'order/cancel'+ id, {
+            headers: authHeader(),
+        });
+    },
+    confirmOrderDelivery(id) {
+        return axios.post(BASE_URL + 'order/delivery/confirm'+ id, {
+            headers: authHeader(),
+        });
+    },
+    commentOrder(data) {
+        return axios.post(BASE_URL + 'order/comment', data, {
+            headers: authHeader(),
+        });
+    },
+    editOrder(data) {
+        return axios.patch(BASE_URL + 'order/edit', data, {
+            headers: authHeader()
+        })
+    },
 }
