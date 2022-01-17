d1363d164ded6229ccc7eec918b848e2ca9aa961
Valeria Stromtsova <v.stromtsova@yandex.ru>
2022-01-17 23:51:39 +0100

    Resolve conflicts x2

--- sandsack-management-frontend/src/store/index.js
+++ sandsack-management-frontend/src/store/index.js
@@ -4,30 +4,18 @@ import users from './modules/users'
 import orders from './modules/orders'
 import equipment from './modules/equipment'
 import auth from './modules/auth'
-<<<<<<< HEAD
 import statisticschart from './modules/statisticschart'
-=======
 import priorities from './modules/priorities'
->>>>>>> development
 
 Vue.use(Vuex)
 
 export default  new Vuex.Store({
-<<<<<<< HEAD
-    modules: {
-        orders,
-        users,
-        auth,
-        equipment,
-        statisticschart
-    }
-=======
   modules: {
     orders,
     users,
     auth,
     equipment,
-    priorities
+    priorities,
+    statisticschart
   }
->>>>>>> development
 })
