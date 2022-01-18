757c2be45cc02cab4f67f439f910b7520fc7cc1a
AlaaEzzeldin <khalila@fim.uni-passau.de>
2022-01-18 01:17:35 +0100

    add function to format the date fetched from backend

--- sandsack-management-frontend/src/mixin/mixin.js
+++ sandsack-management-frontend/src/mixin/mixin.js
@@ -1,22 +1,25 @@
 export const Mixin = {
-    methods: {
-        getColor(status) {
-         if (status === 'ANSTEHEND') return 'grey'
-         else if (status === 'STORNIERT') return 'red'
+        methods: {
+            getColor(status) {
+                if (status === 'ANSTEHEND') return 'grey'
+                else if (status === 'STORNIERT') return 'red'
 
-         else if (status === 'WEITERGELEITET BEI EINSATZABSCHNITT') return 'black'
-         else if (status === 'WEITERGELEITET BEI HAUPTABSCHNITT') return 'black'
+                else if (status === 'WEITERGELEITET BEI EINSATZABSCHNITT') return 'black'
+                else if (status === 'WEITERGELEITET BEI HAUPTABSCHNITT') return 'black'
 
-         else if (status === 'ABGELEHNT BEI EINSATZABSCHNITT') return 'red'
-         else if (status === 'ABGELEHNT BEI HAUPTABSCHNITT') return 'red'
-         else if (status === 'ABGELEHNT BEI EINSATZLEITER') return 'red'
+                else if (status === 'ABGELEHNT BEI EINSATZABSCHNITT') return 'red'
+                else if (status === 'ABGELEHNT BEI HAUPTABSCHNITT') return 'red'
+                else if (status === 'ABGELEHNT BEI EINSATZLEITER') return 'red'
 
-         else if (status === 'AKZEPTIERT') return 'blue'
-         else if (status === 'AUF DEM WEG') return 'orange'
-         else   if (status === 'GELIEFERT') return 'green'
+                else if (status === 'AKZEPTIERT') return 'blue'
+                else if (status === 'AUF DEM WEG') return 'orange'
+                else if (status === 'GELIEFERT') return 'green'
 
+            },
 
-
+            format_time(s) {
+                return new Date(s).toLocaleString()
+            }
         }
     }
-};
+;
