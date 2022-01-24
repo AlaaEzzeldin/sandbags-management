be1df3e1236eeb732575452e579f9956f94649be
AlaaEzzeldin <khalila@fim.uni-passau.de>
2022-01-24 20:22:39 +0100

    adapt the auth-service

--- sandsack-management-frontend/src/services/auth.service.js
+++ sandsack-management-frontend/src/services/auth.service.js
@@ -1,42 +1,29 @@
-import axios from 'axios';
-import {BASE_URL} from "../api/config";
-import authHeader from "./auth-header";
+import api from "./api";
+import TokenService from "./token.service";
 
 class AuthService {
     login(user) {
-        return axios
-            .post(BASE_URL + 'users/login', {
+        return api
+            .post('users/login', {
                 email: user.email,
                 password: user.password
             })
             .then(response => {
                 if (response.data.access_token) {
-                    localStorage.setItem('user', JSON.stringify(response.data));
+                    TokenService.setUser(response.data);
                 }
-
-                return response;
+                return response.data;
             });
     }
 
     logout() {
-        localStorage.removeItem('user');
+        TokenService.removeUser();
     }
 
     updatePassword(data) {
-        return axios
-          .patch(BASE_URL + 'users/change_password', data, {
-              headers: authHeader(),
-          })
+        return api
+          .patch('users/change_password', data)
     }
-
-    /*register(user) {
-        return axios.post(API_URL + 'admin/create_user', {
-            username: user.username,
-            email: user.email,
-            password: user.password
-
-        });
-    }*/
 }
 
 export default new AuthService();
