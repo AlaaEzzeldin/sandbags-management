55464cd80dea8b16612aeec0beab2fd32c228e75
Valeria Stromtsova <v.stromtsova@yandex.ru>
2022-01-25 11:18:19 +0100

    Added footer

--- sandsack-management-frontend/src/App.vue
+++ sandsack-management-frontend/src/App.vue
@@ -5,6 +5,14 @@
             <router-view style="background: white;" :key="$route.path"></router-view>
       </v-container>
     </v-main>
+    <v-footer padless>
+      <v-list-item
+          link
+          :to="{name:'HelpPage'}"
+      >
+        Hilfe
+      </v-list-item>
+    </v-footer>
   </v-app>
 </template>
 
