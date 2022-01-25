c3805ed2ed62d36b1b5dc87542e2d249b6959e39
Valeria Stromtsova <v.stromtsova@yandex.ru>
2022-01-25 11:29:25 +0100

    Applied styling to help footer

--- sandsack-management-frontend/src/App.vue
+++ sandsack-management-frontend/src/App.vue
@@ -5,7 +5,13 @@
             <router-view style="background: white;" :key="$route.path"></router-view>
       </v-container>
     </v-main>
-    <v-footer padless>
+    <v-footer
+        padless
+        app
+        color="grey-lighten-4"
+        v-if="$route.path !== '/help'"
+        style="{z-index: 100}"
+    >
       <v-list-item
           link
           :to="{name:'HelpPage'}"
