93f58c0c0cadf625e9c5f76e5443bf1ff7de4359
Valeria Stromtsova <v.stromtsova@yandex.ru>
2022-01-28 13:21:54 +0100

    Responsiveness of manage equipment page

--- sandsack-management-frontend/src/views/ManageEquipmentPage.vue
+++ sandsack-management-frontend/src/views/ManageEquipmentPage.vue
@@ -1,7 +1,7 @@
 <template>
-  <div v-if="getEquipment.length!==0">
+  <div :class="$vuetify.breakpoint.mdAndUp ? 'pt-10 pl-3 pr-3' : ''" v-if="getEquipment.length!==0">
     <v-row no-gutters>
-      <v-col class="pt-13 justify-center align-center">
+      <v-col class="justify-center align-center">
         <h1 style="font-weight: bolder;">Ausrüstung Zurückgeben</h1>
       </v-col>
     </v-row>
@@ -16,18 +16,18 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col>
+      <v-col :cols="$vuetify.breakpoint.mdAndUp ? 4 : 8">
         <h3>Aktueller Betrag:</h3>
       </v-col>
-      <v-col >
+      <v-col :cols="$vuetify.breakpoint.mdAndUp ? 2 : 4">
         <h3 style="font-weight: normal">{{getCurrentEquipment.quantity}} </h3>
       </v-col>
     </v-row>
     <v-row>
-      <v-col>
+      <v-col :cols="$vuetify.breakpoint.mdAndUp ? 4 : 8">
         <h3>Bitte geben Sie den Betrag ein, der zurückgegeben wurde:</h3>
       </v-col>
-      <v-col>
+      <v-col :cols="$vuetify.breakpoint.mdAndUp ? 2 : 4">
         <v-text-field
             :value="0"
             v-model="newAmount"
@@ -37,7 +37,7 @@
     </v-row>
 
     <v-row>
-      <v-col cols="12" sm="6" offset="3" class="mt-10 pt-10">
+      <v-col cols="12" sm="6" offset="3">
         <v-btn
             style="text-transform: capitalize; font-weight: bolder;"
             rounded
