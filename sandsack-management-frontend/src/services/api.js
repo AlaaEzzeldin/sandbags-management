c9ab87f1769a6e6b3d8606e0df8e590795a69317
Valeria Stromtsova <v.stromtsova@yandex.ru>
2022-01-29 19:46:52 +0100

    Revert "temp- setting the refresh token to 2 mins and change the server to local"
    This reverts commit ec63848a38023fa1f6a0023223ebde00250ccca4.

--- sandsack-management-frontend/src/services/api.js
+++ sandsack-management-frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    //baseURL: "http://18.193.112.34:8001/",
-    baseURL: "http://localhost:8001/",
+    baseURL: "http://18.193.112.34:8001/",
     headers: {
         "Content-Type": "application/json",
     },
